9493cf3b7a93a51073cf6b567bb4d4eeb9201938
Андрей Смирнов <andreysmirnov2k17@yandex.ru>
2025-03-19 12:39:32 +0500

    Finish

--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
 ﻿using System.Diagnostics;
 
-using System;
-using System.Diagnostics;
-using System.IO;
-
 class Program
 {
     static void Main(string[] args)
     {
-        string appPath = @"C:\Users\Andrey\source\repos\CaptchaSolutionByIndusi\bin\Debug\net8.0\CaptchaSolutionByIndusi.exe";
-        string logFilePath = @"C:\Users\Andrey\Desktop\Логи перезапусков.txt";
+        string appPath = @".exe"; // поменять
+        string logFilePath = @".txt"; // поменять
 
         while (true)
         {
@@ -40,7 +36,6 @@ class Program
         {
             string logMessage = $"Перезапуск приложения: {DateTime.Now}\n";
             File.AppendAllText(logFilePath, logMessage);
-            Console.WriteLine($"Время перезапуска записано в файл: {logFilePath}");
         }
         catch (Exception ex)
         {
